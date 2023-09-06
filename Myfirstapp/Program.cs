511617be3a50ab794fc099acf4e1e292775aa2dc
Rando Vanja <randovanikov@gmail.com>
2023-09-06 18:55:20 +0300

    create new app

---
+++ Myfirstapp/Program.cs
@@ -0,0 +1,11 @@
+﻿//rakendus küsin kasutajalt tema nime   
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //output 
+//string - sõne
+string userName = Console.ReadLine(); // input
+
+//Console.WriteLine("Hello, " + userName + "!"); //output
+//string interpolation is a bit harder but better 
+
+Console.WriteLine($"Hello, {userName}!"); // output
